a8b569877020beb4cde828a1cb340086b6860822
houkeanan <hou.keanan@gmail.com>
2026-08-03 09:54:45 +0700

    18th commit

--- src/main/java/com/example/resttemplateserver/book/BookController.java
+++ src/main/java/com/example/resttemplateserver/book/BookController.java
@@ -20,6 +20,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @RequestMapping("/books")
 public class BookController {
 	//ci-cd
+	//sep way ci-cd test
 	private final BookRepository bookRepository;
 
 	public BookController(BookRepository bookRepository) {
